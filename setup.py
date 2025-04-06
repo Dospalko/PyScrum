3e294fe1431eb3527cc2317f83832d1298c4bd86
Dominik Palo <dominik.palo@student.tuke.sk>
2025-04-06 20:17:40 +0200

    added setup

--- setup.py
+++ setup.py
@@ -1,18 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pyscrum",
-    version="0.1.0",
-    description="A minimalist Scrum management library for Python projects.",
-    author="Your Name",
-    author_email="your.email@example.com",
-    packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=[],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+from setuptools import setup
+setup()
